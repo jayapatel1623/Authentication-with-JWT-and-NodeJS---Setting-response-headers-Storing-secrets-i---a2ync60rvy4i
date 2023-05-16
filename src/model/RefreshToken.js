0992fa4a9b7e0382fb69af960541e98e006e7151
Jaya Patel <120316165+jayapatel1623@users.noreply.github.com>
2023-05-16 13:27:31 +0530

    Update RefreshToken.js

--- src/model/RefreshToken.js
+++ src/model/RefreshToken.js
@@ -1,1 +1,13 @@
+const mongooose = require('mongoose');
 
+const tokenSchema = new mongooose.Schema({
+    // Your code goes here
+    token: {
+    type: String,
+    required: true,
+  },
+});
+
+const RefreshToken = mongooose.model('tokens', tokenSchema);
+
+module.exports = RefreshToken;
